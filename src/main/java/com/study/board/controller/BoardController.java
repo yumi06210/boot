905c7407790d869581007e5c52f662fea63e1506
yumi06210 <letter06210@gmail.com>
2023-07-13 14:46:06 +0900

    프로시저로 테스트데이터 생성후 타임리프를 통해 뷰단에서 리스트 출력

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.controller.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,7 +28,12 @@ public class BoardController {
         //System.out.println("제목 : "+title);
         //System.out.println("내용 : "+content);
         return "";
+    }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
 
+        return "boardlist";
     }
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -6,13 +6,16 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
     private BoardRepository boardRepository; //new 인스턴스 생성하는데 대신에 Autowired 사용
     public void write(Board board){
-
-
         boardRepository.save(board);
+    }
+    public List<Board> boardList(){
+        return boardRepository.findAll();  //리턴받은 모든 값을 반환
     }
 }
