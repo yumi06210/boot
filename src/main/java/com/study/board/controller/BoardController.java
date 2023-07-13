d4e2c23e9f7c1bf0c31077e5c5f401c013652624
yumi06210 <letter06210@gmail.com>
2023-07-13 16:30:39 +0900

    modify  화면이 404 에러난다. 리스트 버튼은 혼자 만들어봄

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -2,10 +2,12 @@ package com.study.board.controller;
 
 import com.study.board.controller.entity.Board;
 import com.study.board.service.BoardService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -32,7 +34,6 @@ public class BoardController {
     @GetMapping("/board/list")
     public String boardList(Model model){
         model.addAttribute("list", boardService.boardList());
-
         return "boardlist";
     }
 
@@ -47,6 +48,12 @@ public class BoardController {
         boardService.boardDelete(id);
         //삭제하면 리스트 페이지로 감
         return "redirect:/board/list";
+    }
+
+
+    @GetMapping("/board/modify/{id}")
+    public String boardmodify(@PathVariable("id") Integer id){
+        return "boardmodify";
     }
 
 }
